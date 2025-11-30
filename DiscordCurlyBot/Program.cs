6d2241badce9e29b051c7b7a4c91fa72e4538b79
Иван Владимирович Мякенький <myakivan@gmail.com>
2025-11-30 15:05:36 +0300

    Добавьте файлы проекта.

---
+++ DiscordCurlyBot/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string Token = "MTQ0NDY1Mzc1NzI2NTczOTc4Ng.GOpYGd.rTxo6z0CqPhXR_D1RaX9sesJ7JzFSFnnYKYljQ";
