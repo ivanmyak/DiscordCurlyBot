db804e666fa2837d9e17a3960cf226389b05aa3c
Иван Владимирович Мякенький <myakivan@gmail.com>
2025-12-03 12:47:41 +0300

    feat: Оживил бота;  *Добавил Интерфейс и Класс для бота  *Добавил секреты пользователя и переписал токен в секреты  *Пока сделал использование команд бота через ! (вместо \)

---
+++ DiscordCurlyBot/Classes/Bot.cs
@@ -0,0 +1,81 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordCurlyBot.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordCurlyBot.Classes
+{
+    internal class Bot : IBot
+    {
+        private ServiceProvider? _serviceProvider;
+
+        private readonly ILogger<Bot> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
+        public Bot(ILogger<Bot> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+
+            DiscordSocketConfig socketConfig = new()
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            };
+
+            _client = new DiscordSocketClient(socketConfig);
+            _commands = new CommandService();
+        }
+
+        public async Task StartAsync(ServiceProvider serviceProvider)
+        {
+            string discordToken = _configuration["DiscordToken"] ?? throw new Exception("Missing Discord Token!");
+            _logger.LogInformation($"Starting with token {discordToken}");
+
+            await _commands.AddModulesAsync(Assembly.GetExecutingAssembly(), _serviceProvider);
+
+            await _client.LoginAsync(TokenType.Bot, discordToken);
+            await _client.StartAsync();
+
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        public Task StopASync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            if (arg is not SocketUserMessage message || message.Author.IsBot)
+            {
+                return;
+            }
+
+            _logger.LogInformation($"{DateTime.Now.ToShortTimeString()} = {message.Author}: {message.Content}");
+
+
+            int position = 0;
+            bool messageIsCommand = message.HasCharPrefix('!', ref position);
+
+            if (messageIsCommand)
+            {
+                await _commands.ExecuteAsync(
+                    new SocketCommandContext(_client, message),
+                    position,
+                    _serviceProvider);
+            }
+        }
+    }
+}
--- DiscordCurlyBot/Program.cs
+++ DiscordCurlyBot/Program.cs
@@ -1,5 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+using Discord;
+using Discord.WebSocket;
+using DiscordCurlyBot.Classes;
+using DiscordCurlyBot.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 
-string Token = "MTQ0NDY1Mzc1NzI2NTczOTc4Ng.GOpYGd.rTxo6z0CqPhXR_D1RaX9sesJ7JzFSFnnYKYljQ";
+
+internal class Program
+{
+    private static DiscordSocketClient _client;
+
+    private static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+
+    private static async Task MainAsync(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets(Assembly.GetExecutingAssembly())
+            .Build();
+
+        var serviceProvider = new ServiceCollection()
+            .AddLogging(options =>
+            {
+                options.ClearProviders();
+                options.AddConsole();
+            })
+            .AddSingleton<IConfiguration>(configuration)
+            .AddScoped<IBot, Bot>()
+            .BuildServiceProvider();
+
+        try
+        {
+            IBot bot = serviceProvider.GetRequiredService<IBot>();
+            await bot.StartAsync(serviceProvider);
+
+            while (true)
+            {
+                var keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.Q)
+                {
+                    await bot.StopASync();
+                    return;
+                }
+            }
+        }
+        catch (Exception exeption)
+        {
+            Console.WriteLine(exeption.Message);
+            Environment.Exit(-1);
+        }
+
+    }
+
+}
